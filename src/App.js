3de9dcac889bca9c033b47b6209c9d15d7d76572
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-05-27 12:44:14 +0300

    Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import Wrapper from "components/Wrapper";
+import Section from "components/Section";
+// import styles from './services/styles'
+
+function App() {
+
+    return (
+        <Wrapper>
+            <Section>
+            </Section>
+        </Wrapper>
+    )
+}
+
+export default App;
