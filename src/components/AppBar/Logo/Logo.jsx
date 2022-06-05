4392844f76f5c207b4c5f49b4e41a26515537aac
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-06-05 16:58:24 +0300

    Fixed_Logo

--- src/components/AppBar/Logo/Logo.jsx
+++ src/components/AppBar/Logo/Logo.jsx
@@ -3,10 +3,10 @@ import sprite from '../../../images/icons.svg'
 import s from './Logo.module.css';
 
 export default function Logo({link}) {
-    return <NavLink className={s.logo} to={link}>
-        <svg className={s.logo__icon} width="24" height="24" aria-label="logo">
-          <use href={`${sprite}#logo`}></use>
-        </svg>
-        <p className={s.logo__text}>Filmoteka</p>
-      </NavLink>
+  return <NavLink className={s.logo} to={link}>
+    <svg className={s.icon} width="24" height="24" aria-label="logo">
+      <use href={`${sprite}#logo`}></use>
+    </svg>
+    <p className={s.text}>Filmoteka</p>
+  </NavLink>
 }
